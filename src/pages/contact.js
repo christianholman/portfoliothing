ea51e2202881caaf35568fc215a7b19b4b894bd6
Christian Holman <chrisholm2@yahoo.com>
2019-07-28 19:16:56 +0200

    bleh

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,10 +3,16 @@ import React from "react"
 import Layout from '../components/layout'
 import SocialMedia from "../components/socialmedia";
 
+import SEO from '../components/seo'
 export default class ContactPage extends React.Component {
     render() {
         return (
             <Layout>
+                <SEO
+                    description="Christian Holman contact details and social medias."
+                    title="Christian Holman - Contact"
+                    lang="en"
+                />
                 <span className="text-xl sm:text-2xl text-blackish"><span className="underlined font-bold">Get in touch</span></span>
                 <p className="text-lg sm:text-xl text-blackish font-light mt-2">
                     If you would like to work with me, or speak with me for any reason, feel free to hit me up on any social media below or by e-mail at <a className="underlined underlined-blue" href="mailto:chrisholm2@yahoo.com">chrisholm2@yahoo.com</a>
