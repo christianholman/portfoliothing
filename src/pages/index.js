300d44ecf2f3826176c432f0eee676d27899d38f
Christian Holman <chrisholm2@yahoo.com>
2019-10-24 09:11:38 +0200

    Updated twitter link in bio

--- src/pages/index.js
+++ src/pages/index.js
@@ -29,7 +29,7 @@ const IndexPage = ({ data }) => (
       <span class="w-full text-xl sm:text-2xl text-blackish">Hi, I'm <span className="underlined font-bold">Christian Holman</span></span>
       <p className="text-lg sm:text-xl text-blackish font-light mt-2">
           I am a high-school student, developer, AI-advocate, and aspiring entrepeneur. On this site you can find my <Link to="/projects" className="underlined underlined-blue">projects</Link>, <Link to="/journal" className="underlined underlined-blue">journal</Link>, and <Link to="/contact" className="underlined underlined-blue">contact information</Link>.
-          I am currently focusing on school and working on side-projects. If you want to work with me, shoot me a message on <a href="http://twitter.com/bigluxio" className="underlined underlined-blue" target="_blank">twitter</a> or get in touch via my contact page.
+          I am currently focusing on school and working on side-projects. If you want to work with me, shoot me a message on <a href="http://twitter.com/0xholman" className="underlined underlined-blue" target="_blank">twitter</a> or get in touch via my contact page.
           
           
           <br/>
